6c7d62f02b3f5081127e23d2c6ce4f76b3541719
Kevin Olivier <kevinolivier1111@gmail.com>
2022-11-15 17:23:42 -0700

    created starter database

---
+++ cuisine-server/db/migrate/20221115205641_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t| 
+      t.string :name
+      
+      
+      
+      
+      
+      t.belongs_to :user
+    end
+
+      
+  end
+end
