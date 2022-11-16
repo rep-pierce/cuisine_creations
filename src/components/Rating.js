fbbc454a4ed73be23d224d20bdb4fda1a743e1bc
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-11-16 13:49:42 -0700

    update

---
+++ src/components/Rating.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function RecipeCard () {
+
+    return(<div></div>)
+}
+
+export default RecipeCard;
---
+++ src/components/Recipepage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Recipepage () {
+
+    return(<div></div>)
+}
+
+export default Recipepage;
