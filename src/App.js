3a7552e9532f05b9752365946ab508c1c80ff984
Kevin Olivier <kevinolivier1111@gmail.com>
2022-11-16 14:04:44 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import HomePage from "./components/Homepage";
 import LoginPage from "./components/Loginpage";
 import ListPage from "./components/Listpage";
 import Recipepage from "./components/Recipepage";
-
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route exact path = "/" element={<HomePage />}/>
+        <Route exact path = "/login" element={<LoginPage />}/>
+        <Route exact path = "/recipe" element={<Recipepage />}/>
+        <Route exact path = "/list" element={<ListPage />}/>
+      </Routes>
     </div>
   );
 }
