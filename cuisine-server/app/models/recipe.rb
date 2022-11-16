318ca22824c43d0ded1f04bbfba74f7bdd947e4d
Ryan Pierce <112719480+rep-pierce@users.noreply.github.com>
2022-11-16 09:21:16 -0700

    Update recipe.rb

--- cuisine-server/app/models/recipe.rb
+++ cuisine-server/app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
     belongs_to :user
     has_many :joinirs
     has_many :ingredients, through: :joinirs
+    has_many :users, through: :lists
 
     def recipe_ingredients
         self.joinirs.map(&:list_ingredients)
