4f10c30f005967f90b62231c83198a7202ff5b93
Kevin Olivier <kevinolivier1111@gmail.com>
2022-11-16 20:20:14 -0700

    update

--- src/components/Usercard.js
+++ src/components/Usercard.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function UserCard () {
+function UserCard ({user}) {
+    const [password, setPassword] = useState('')
 
-    return(<div></div>)
+    function handleSubmit(e) {
+        e.preventDefualt()
+        fetch(`http://localhost:9292/Users/${user.id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              password: password
+              
+            }),
+          })
+            
+    }
+
+    return(
+    <div className ="userCard">
+        <p>Name:{user.name}</p>
+        <p>Age:{user.age}</p>
+        <p>Username:{user.username}</p>
+        <form onSubmit={handleSubmit}>
+            <label>
+                <p>Change Password</p>
+                <input
+                value = {password}
+                type ="text"
+                placeholder = "New Password"
+                onChange={(e)=> setPassword(e.target.value)}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    )
 }
 
 export default UserCard;
